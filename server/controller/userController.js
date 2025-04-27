8a33713e2669a9e2eecc853be703e9e9d82d202d
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-04-27 20:18:53 +0530

    get user with specific id

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -26,3 +26,16 @@ export const getAllUsers = async (req, res) => {
     res.status(500).json({ errorMessage: error.message });
   }
 };
+
+export const getUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(userExist);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { create, getAllUsers } from '../controller/userController.js'
+import { create, getAllUsers, getUserById } from '../controller/userController.js'
 
 const route = express.Router();
 
 route.post("/user",create);
 route.get("/users", getAllUsers);
+route.get("/user/:id",getUserById);
 
 export default route;
