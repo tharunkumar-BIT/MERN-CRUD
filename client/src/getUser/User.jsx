e1aa84636668ce8f18598d8ca03c398f9a0d2056
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-04-28 18:15:09 +0530

    delete function

--- client/src/getUser/User.jsx
+++ client/src/getUser/User.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./User.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const User = () => {
   const [users, setUsers] = useState([]);
@@ -16,6 +17,18 @@ const User = () => {
     };
     fetchData();
   }, []);
+
+  const deleteUser = async (userId) => {
+    axios
+      .delete(`http://localhost:8000/api/delete/user/${userId}`)
+      .then((response) => {
+        setUsers((prevUser) => prevUser.filter((user) => user._id !== userId));
+        toast.success(response.data.message, { position: "top-right" });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="userTable">
@@ -42,11 +55,15 @@ const User = () => {
                   <td>{user.email}</td>
                   <td>{user.address}</td>
                   <td className="actionButtons">
-                    <Link to={`update/` + user._id} type="button" className="btn btn-info">
+                    <Link
+                      to={`update/` + user._id}
+                      type="button"
+                      className="btn btn-info"
+                    >
                       <i className="fa-solid fa-pen-to-square"></i>{" "}
                     </Link>
 
-                    <button type="button" className="btn btn-danger">
+                    <button onClick={()=> deleteUser(user._id)} type="button" className="btn btn-danger">
                       <i className="fa-solid fa-trash"></i>
                     </button>
                   </td>
